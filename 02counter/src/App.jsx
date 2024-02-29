1192c9df29a88eb430c7cf5b07045fe703b19736
Ujjwal_Kumar <kingayushman7@gmail.com>
2024-02-28 23:15:58 -0800

    interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,14 +5,18 @@ import './App.css'
 
 function App() {
 
-  let [counter, setCounter] = useState(15)
+
+  const [counter, setCounter] = useState(15)
 
   // let counter = 15
 
   const addValue = () => {
 
-    counter = counter + 1
-    setCounter(counter)
+    // counter = counter + 1
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
 
   const removeValue = () => {
